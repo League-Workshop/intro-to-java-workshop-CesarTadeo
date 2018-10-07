995c5a2b035e06ad723b04c64fd6b2598c0d0b68
league <league@wts5.attlocal.net>
2018-10-07 11:53:57 -0700

    Today I learned how to make my robot make a spiral, pentagon and i am starting to make a triangle shell.

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,20 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot BB8 = new Robot();
 		// 5. Set your robot's pen down 
-		
+		BB8.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		BB8.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for(int i= 0 ;i< 75 ; i++) {
 			// 7. Change the pen color to random
-		
+		BB8.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			BB8.move (5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		BB8.turn(360/7);
 			// 8. Set the pen width to i
-			
+			BB8.setPenWidth(i);
 	}
-}
+}}
--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -5,32 +5,37 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+Robot bb8 = new Robot();
 	
 	void go() {
 		drawTriangle(100); // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+		bb8.setSpeed(100);
 		// 4. make a variable to hold the length of the triangle and set it to 50
 
 		// 7. Use a for loop to repeat steps #9 to #10, 60 times
 
 			// 9. Change the color of the pen to a random color
-	
+	bb8.setRandomPenColor();
 			// 8. Increase the length variable by 10
 	
 			// 5. call your drawTriangle() method using your length variable
 	
 			// 10. Turn the robot 6 degrees to the right
-
+		
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
 	private void drawTriangle(int length) {
-		
-		
-		
+		for(int i= 0 ;i< 999999999 ; i++) {
+	bb8.move	(length);
+	bb8.turn(60);
+	bb8.move(length);
+	bb8.turn(60);
+	bb8.move(length);
+	bb8.turn(60);
+		}
 	}
 
 	
