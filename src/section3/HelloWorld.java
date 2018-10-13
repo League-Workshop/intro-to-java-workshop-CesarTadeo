abe586f2a787fc4c46635f067fba2a48b6f2b234
CesarTadeo <league@wts5.attlocal.net>
2018-10-13 11:51:37 -0700

    Workshop 3

---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,12 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("HelloWorld");
+	JOptionPane.showMessageDialog(null, "HI CESAR");
+	JOptionPane.showInputDialog("Hello, how are you");
+}
+	
+}
---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+public static void main(String[] args) {
+	String name;
+	name=JOptionPane.showInputDialog("What's is your name");
+	JOptionPane.showMessageDialog(null, name+", nice to meet you");
+	
+}
+	
+}
